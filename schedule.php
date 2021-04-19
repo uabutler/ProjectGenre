8ad87bab2cc4bd9a15fc848d9fe136ca93f9b208
Ulysses Butler <ulysses@uabutler.com>
2021-04-19 01:42:55 -0500

    Created schedule calendar

--- schedule.php
+++ schedule.php
@@ -24,7 +24,7 @@
         </button>
     </div>
     <div class="clearfix"></div>
-    <table class="ui fixed celled table">
+    <table class="ui fixed celled table schedule-calendar">
         <thead>
         <tr>
             <th>Sunday</th>
@@ -38,13 +38,139 @@
         </thead>
         <tbody>
         <tr>
-            <td>1</td>
-            <td>2</td>
-            <td>3</td>
-            <td>4</td>
-            <td>5</td>
-            <td>6</td>
-            <td>7</td>
+            <td>
+                <span class="calendar-date-number other-month">28</span>
+            </td>
+            <td>
+                <span class="calendar-date-number other-month">29</span>
+            </td>
+            <td>
+                <span class="calendar-date-number other-month">30</span>
+            </td>
+            <td>
+                <span class="calendar-date-number other-month">31</span>
+            </td>
+            <td>
+                <span class="calendar-date-number">1</span>
+            </td>
+            <td>
+                <span class="calendar-date-number">2</span>
+            </td>
+            <td>
+                <span class="calendar-date-number">3</span>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <span class="calendar-date-number">4</span>
+            </td>
+            <td>
+                <span class="calendar-date-number">5</span>
+            </td>
+            <td>
+                <span class="calendar-date-number">
+                    6
+                    <a href="#">
+                    <div class="ui message meeting-info-box">
+                        <div class="header">
+                            Alice in Wonderland
+                        </div>
+                        <p>3:00pm - 5:00pm</p>
+                    </div>
+                    </a>
+                </span>
+            </td>
+            <td>
+                <span class="calendar-date-number">7</span>
+            </td>
+            <td>
+                <span class="calendar-date-number">8</span>
+            </td>
+            <td>
+                <span class="calendar-date-number">9</span>
+            </td>
+            <td>
+                <span class="calendar-date-number">10</span>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <span class="calendar-date-number">11</span>
+            </td>
+            <td>
+                <span class="calendar-date-number">12</span>
+            </td>
+            <td>
+                <span class="calendar-date-number">13</span>
+            </td>
+            <td>
+                <span class="calendar-date-number">14</span>
+            </td>
+            <td>
+                <span class="calendar-date-number">15</span>
+            </td>
+            <td>
+                <span class="calendar-date-number">16</span>
+            </td>
+            <td>
+                <span class="calendar-date-number">17</span>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <span class="calendar-date-number">18</span>
+            </td>
+            <td>
+                <span class="calendar-date-number">19</span>
+            </td>
+            <td>
+                <span class="calendar-date-number">
+                    20
+                    <a href="#">
+                    <div class="ui message meeting-info-box">
+                        <div class="header">
+                            Alice in Wonderland
+                        </div>
+                        <p>3:00pm - 5:00pm</p>
+                    </div>
+                    </a>
+                </span>
+            </td>
+            <td>
+                <span class="calendar-date-number">21</span>
+            </td>
+            <td>
+                <span class="calendar-date-number">22</span>
+            </td>
+            <td>
+                <span class="calendar-date-number">23</span>
+            </td>
+            <td>
+                <span class="calendar-date-number">24</span>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <span class="calendar-date-number">25</span>
+            </td>
+            <td>
+                <span class="calendar-date-number">26</span>
+            </td>
+            <td>
+                <span class="calendar-date-number">27</span>
+            </td>
+            <td>
+                <span class="calendar-date-number">28</span>
+            </td>
+            <td>
+                <span class="calendar-date-number">29</span>
+            </td>
+            <td>
+                <span class="calendar-date-number">30</span>
+            </td>
+            <td>
+                <span class="calendar-date-number other-month">1</span>
+            </td>
         </tr>
         </tbody>
     </table>
