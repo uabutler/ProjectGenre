38a01a775e6ff0179a9120a05c1552e2d519b7b0
Ulysses Butler <ulysses@uabutler.com>
2021-04-21 01:17:16 -0500

    Added friends landing page

---
+++ friends-ex.php
@@ -0,0 +1,258 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php require 'common/head.php'; ?>
+    <title>Project Genre</title>
+</head>
+<body>
+<header>
+    <?php require 'common/header.php'; createHeader("Friends"); ?>
+</header>
+<section>
+    <div class="ui grid">
+        <div class="four wide column">
+            <h1 class="ui header">Friends List</h1>
+            <div class="ui vertical pointing menu">
+                <a class="item">
+                    <img class="ui avatar image" src="/assets/avatar.png">
+                    <span>John Doe</span>
+                </a>
+                <a class="active item">
+                    <img class="ui avatar image" src="/assets/avatar1.png">
+                    <span>Bob Brown</span>
+                </a>
+                <a class="item">
+                    <img class="ui avatar image" src="/assets/avatar2.png">
+                    <span>Charlie Chapel</span>
+                </a>
+                <a class="item">
+                    <img class="ui avatar image" src="/assets/avatar3.png">
+                    <span>Dave David</span>
+                </a>
+                <a class="item">
+                    <img class="ui avatar image" src="/assets/avatar.png">
+                    <span>John Doe</span>
+                </a>
+                <a class="item" href="friends-ex.php">
+                    <img class="ui avatar image" src="/assets/avatar1.png">
+                    <span>Bob Brown</span>
+                </a>
+                <a class="item">
+                    <img class="ui avatar image" src="/assets/avatar2.png">
+                    <span>Charlie Chapel</span>
+                </a>
+                <a class="item">
+                    <img class="ui avatar image" src="/assets/avatar3.png">
+                    <span>Dave David</span>
+                </a>
+            </div>
+        </div>
+        <div class="twelve wide column">
+            <img class="circular image" src="assets/avatar1.png" style="float: left; width: 25%">
+            <div style="float: left; padding-left: 20px;">
+                <h2 style="text-decoration: underline; font-variant-caps: small-caps;">Bob Brown</h2>
+                <p>
+                    This is a brief biography about this person and their interest that they provide when creating their profile
+                </p>
+                <h3 style="margin-bottom: 5px">Interests</h3>
+                <p>Romance, Science Fiction</p>
+                <h3 style="margin-bottom: 5px">Favorite Books</h3>
+                <p>Brave New World, 1984, Alice in Wonderland</p>
+            </div>
+            <div class="clearfix"></div>
+            <h2 class="ui header" style="margin-top: 30px;">Schedule</h2>
+            <table class="ui fixed celled table schedule-calendar">
+                <thead>
+                <tr>
+                    <th>Sunday</th>
+                    <th>Monday</th>
+                    <th>Tuesday</th>
+                    <th>Wednesday</th>
+                    <th>Thursday</th>
+                    <th>Friday</th>
+                    <th>Saturday</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>
+                        <span class="calendar-date-number other-month">28</span>
+                    </td>
+                    <td>
+                        <span class="calendar-date-number other-month">29</span>
+                    </td>
+                    <td>
+                        <span class="calendar-date-number other-month">30</span>
+                    </td>
+                    <td>
+                        <span class="calendar-date-number other-month">31</span>
+                    </td>
+                    <td>
+                        <span class="calendar-date-number">1</span>
+                    </td>
+                    <td>
+                        <span class="calendar-date-number">2</span>
+                    </td>
+                    <td>
+                        <span class="calendar-date-number">
+                            3
+                            <a href="#">
+                            <div class="ui message meeting-info-box">
+                                <div class="header">
+                                    1984
+                                </div>
+                                <p>12:00 - 16:00</p>
+                            </div>
+                            </a>
+                        </span>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <span class="calendar-date-number">4</span>
+                    </td>
+                    <td>
+                        <span class="calendar-date-number">5</span>
+                    </td>
+                    <td>
+                        <span class="calendar-date-number">6</span>
+                    </td>
+                    <td>
+                        <span class="calendar-date-number">7</span>
+                    </td>
+                    <td>
+                        <span class="calendar-date-number">8</span>
+                    </td>
+                    <td>
+                        <span class="calendar-date-number">9</span>
+                    </td>
+                    <td>
+                        <span class="calendar-date-number">
+                            10
+                            <a href="#">
+                            <div class="ui message meeting-info-box">
+                                <div class="header">
+                                    1984
+                                </div>
+                                <p>12:00 - 16:00</p>
+                            </div>
+                            </a>
+                        </span>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <span class="calendar-date-number">11</span>
+                    </td>
+                    <td>
+                        <span class="calendar-date-number">12</span>
+                    </td>
+                    <td>
+                        <span class="calendar-date-number">13</span>
+                    </td>
+                    <td>
+                        <span class="calendar-date-number">
+                            14
+                            <a href="#">
+                            <div class="ui message meeting-info-box">
+                                <div class="header">
+                                    Brave New World
+                                </div>
+                                <p>13:00 - 15:00</p>
+                            </div>
+                            </a>
+                        </span>
+                    </td>
+                    <td>
+                        <span class="calendar-date-number">15</span>
+                    </td>
+                    <td>
+                        <span class="calendar-date-number">16</span>
+                    </td>
+                    <td>
+                        <span class="calendar-date-number">
+                            17
+                            <a href="#">
+                            <div class="ui message meeting-info-box">
+                                <div class="header">
+                                    1984
+                                </div>
+                                <p>12:00 - 16:00</p>
+                            </div>
+                            </a>
+                        </span>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <span class="calendar-date-number">18</span>
+                    </td>
+                    <td>
+                        <span class="calendar-date-number">19</span>
+                    </td>
+                    <td>
+                        <span class="calendar-date-number">20</span>
+                    </td>
+                    <td>
+                        <span class="calendar-date-number">21</span>
+                    </td>
+                    <td>
+                        <span class="calendar-date-number">22</span>
+                    </td>
+                    <td>
+                        <span class="calendar-date-number">23</span>
+                    </td>
+                    <td>
+                        <span class="calendar-date-number">
+                            24
+                            <a href="#">
+                            <div class="ui message meeting-info-box">
+                                <div class="header">
+                                    1984
+                                </div>
+                                <p>12:00 - 16:00</p>
+                            </div>
+                            </a>
+                        </span>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <span class="calendar-date-number">25</span>
+                    </td>
+                    <td>
+                        <span class="calendar-date-number">26</span>
+                    </td>
+                    <td>
+                        <span class="calendar-date-number">27</span>
+                    </td>
+                    <td>
+                        <span class="calendar-date-number">
+                            28
+                            <a href="#">
+                            <div class="ui message meeting-info-box">
+                                <div class="header">
+                                    Brave New World
+                                </div>
+                                <p>13:00 - 15:00</p>
+                            </div>
+                            </a>
+                        </span>
+                    </td>
+                    <td>
+                        <span class="calendar-date-number">29</span>
+                    </td>
+                    <td>
+                        <span class="calendar-date-number">30</span>
+                    </td>
+                    <td>
+                        <span class="calendar-date-number other-month">1</span>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</section>
+</body>
+</html>
--- friends.php
+++ friends.php
@@ -10,13 +10,30 @@
 </header>
 <section>
     <div class="ui grid">
-        <div class="four wide column">
+        <div class="eight wide column">
+            <h1 class="ui header">Friends List</h1>
             <div class="ui vertical pointing menu">
                 <a class="item">
                     <img class="ui avatar image" src="/assets/avatar.png">
                     <span>John Doe</span>
                 </a>
-                <a class="active item">
+                <a class="item" href="friends-ex.php">
+                    <img class="ui avatar image" src="/assets/avatar1.png">
+                    <span>Bob Brown</span>
+                </a>
+                <a class="item">
+                    <img class="ui avatar image" src="/assets/avatar2.png">
+                    <span>Charlie Chapel</span>
+                </a>
+                <a class="item">
+                    <img class="ui avatar image" src="/assets/avatar3.png">
+                    <span>Dave David</span>
+                </a>
+                <a class="item">
+                    <img class="ui avatar image" src="/assets/avatar.png">
+                    <span>John Doe</span>
+                </a>
+                <a class="item" href="friends-ex.php">
                     <img class="ui avatar image" src="/assets/avatar1.png">
                     <span>Bob Brown</span>
                 </a>
@@ -30,210 +47,62 @@
                 </a>
             </div>
         </div>
-        <div class="twelve wide column">
-            <img class="circular image" src="assets/avatar1.png" style="float: left; width: 25%">
-            <div style="float: left; padding-left: 20px;">
-                <h2 style="text-decoration: underline; font-variant-caps: small-caps;">Bob Brown</h2>
-                <p>
-                    This is a brief biography about this person and their interest that they provide when creating their profile
-                </p>
-                <h3 style="margin-bottom: 5px">Interests</h3>
-                <p>Romance, Science Fiction</p>
-                <h3 style="margin-bottom: 5px">Favorite Books</h3>
-                <p>Brave New World, 1984, Alice in Wonderland</p>
+        <div class="eight wide column">
+            <h1 class="ui header">Find Friends</h1>
+            <form class="ui form">
+                <div class="field">
+                    <label>Search</label>
+                    <input type="text" placeholder="Find friends">
+                </div>
+                <div class="field">
+                    <label>Planning To Read</label>
+                    <select multiple="" class="ui search dropdown">
+                        <option value="">Select a book</option>
+                        <option value="1984">1984</option>
+                        <option value="at">A Tale of Two Cities</option>
+                        <option value="aw">Alice in Wonderland</option>
+                        <option value="bnw">Brave New World</option>
+                        <option value="f">Fahrenheit 451</option>
+                        <option value="m">Moby Dick</option>
+                    </select>
+                </div>
+                <button class="ui right floated button">Search</button>
+            </form>
+            <h1 class="ui header clearfix">Recommended Friends</h1>
+            <div class="ui vertical pointing menu">
+                <a class="item">
+                    <button class="mini ui icon button" style="float:right;">
+                        <i class="plus icon"></i>
+                    </button>
+                    <img class="ui avatar image" src="/assets/avatar.png">
+                    <span style="text-decoration: underline;">John Doe</span>
+                    <p>Reading List: <span class="people-list-info">Brave New World</span></p>
+                </a>
+                <a class="item" href="friends-ex.php">
+                    <button class="mini ui icon button" style="float:right;">
+                        <i class="plus icon"></i>
+                    </button>
+                    <img class="ui avatar image" src="/assets/avatar1.png">
+                    <span style="text-decoration: underline;">Bob Brown</span>
+                    <p>Reading List: <span class="people-list-info">1984, Alice in Wonderland</span></p>
+                </a>
+                <a class="item">
+                    <button class="mini ui icon button" style="float:right;">
+                        <i class="plus icon"></i>
+                    </button>
+                    <img class="ui avatar image" src="/assets/avatar2.png">
+                    <span style="text-decoration: underline;">Charlie Chapel</span>
+                    <p>Reading List: <span class="people-list-info">Tale of Two Cities</span></p>
+                </a>
+                <a class="item">
+                    <button class="mini ui icon button" style="float:right;">
+                        <i class="plus icon"></i>
+                    </button>
+                    <img class="ui avatar image" src="/assets/avatar3.png">
+                    <span style="text-decoration: underline;">Dave David</span>
+                    <p>Reading List: <span class="people-list-info">Moby Dick</span></p>
+                </a>
             </div>
-            <div class="clearfix"></div>
-            <h2 class="ui header" style="margin-top: 30px;">Schedule</h2>
-            <table class="ui fixed celled table schedule-calendar">
-                <thead>
-                <tr>
-                    <th>Sunday</th>
-                    <th>Monday</th>
-                    <th>Tuesday</th>
-                    <th>Wednesday</th>
-                    <th>Thursday</th>
-                    <th>Friday</th>
-                    <th>Saturday</th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr>
-                    <td>
-                        <span class="calendar-date-number other-month">28</span>
-                    </td>
-                    <td>
-                        <span class="calendar-date-number other-month">29</span>
-                    </td>
-                    <td>
-                        <span class="calendar-date-number other-month">30</span>
-                    </td>
-                    <td>
-                        <span class="calendar-date-number other-month">31</span>
-                    </td>
-                    <td>
-                        <span class="calendar-date-number">1</span>
-                    </td>
-                    <td>
-                        <span class="calendar-date-number">2</span>
-                    </td>
-                    <td>
-                        <span class="calendar-date-number">
-                            3
-                            <a href="#">
-                            <div class="ui message meeting-info-box">
-                                <div class="header">
-                                    1984
-                                </div>
-                                <p>12:00 - 16:00</p>
-                            </div>
-                            </a>
-                        </span>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <span class="calendar-date-number">4</span>
-                    </td>
-                    <td>
-                        <span class="calendar-date-number">5</span>
-                    </td>
-                    <td>
-                        <span class="calendar-date-number">6</span>
-                    </td>
-                    <td>
-                        <span class="calendar-date-number">7</span>
-                    </td>
-                    <td>
-                        <span class="calendar-date-number">8</span>
-                    </td>
-                    <td>
-                        <span class="calendar-date-number">9</span>
-                    </td>
-                    <td>
-                        <span class="calendar-date-number">
-                            10
-                            <a href="#">
-                            <div class="ui message meeting-info-box">
-                                <div class="header">
-                                    1984
-                                </div>
-                                <p>12:00 - 16:00</p>
-                            </div>
-                            </a>
-                        </span>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <span class="calendar-date-number">11</span>
-                    </td>
-                    <td>
-                        <span class="calendar-date-number">12</span>
-                    </td>
-                    <td>
-                        <span class="calendar-date-number">13</span>
-                    </td>
-                    <td>
-                        <span class="calendar-date-number">
-                            14
-                            <a href="#">
-                            <div class="ui message meeting-info-box">
-                                <div class="header">
-                                    Brave New World
-                                </div>
-                                <p>13:00 - 15:00</p>
-                            </div>
-                            </a>
-                        </span>
-                    </td>
-                    <td>
-                        <span class="calendar-date-number">15</span>
-                    </td>
-                    <td>
-                        <span class="calendar-date-number">16</span>
-                    </td>
-                    <td>
-                        <span class="calendar-date-number">
-                            17
-                            <a href="#">
-                            <div class="ui message meeting-info-box">
-                                <div class="header">
-                                    1984
-                                </div>
-                                <p>12:00 - 16:00</p>
-                            </div>
-                            </a>
-                        </span>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <span class="calendar-date-number">18</span>
-                    </td>
-                    <td>
-                        <span class="calendar-date-number">19</span>
-                    </td>
-                    <td>
-                        <span class="calendar-date-number">20</span>
-                    </td>
-                    <td>
-                        <span class="calendar-date-number">21</span>
-                    </td>
-                    <td>
-                        <span class="calendar-date-number">22</span>
-                    </td>
-                    <td>
-                        <span class="calendar-date-number">23</span>
-                    </td>
-                    <td>
-                        <span class="calendar-date-number">
-                            24
-                            <a href="#">
-                            <div class="ui message meeting-info-box">
-                                <div class="header">
-                                    1984
-                                </div>
-                                <p>12:00 - 16:00</p>
-                            </div>
-                            </a>
-                        </span>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <span class="calendar-date-number">25</span>
-                    </td>
-                    <td>
-                        <span class="calendar-date-number">26</span>
-                    </td>
-                    <td>
-                        <span class="calendar-date-number">27</span>
-                    </td>
-                    <td>
-                        <span class="calendar-date-number">
-                            28
-                            <a href="#">
-                            <div class="ui message meeting-info-box">
-                                <div class="header">
-                                    Brave New World
-                                </div>
-                                <p>13:00 - 15:00</p>
-                            </div>
-                            </a>
-                        </span>
-                    </td>
-                    <td>
-                        <span class="calendar-date-number">29</span>
-                    </td>
-                    <td>
-                        <span class="calendar-date-number">30</span>
-                    </td>
-                    <td>
-                        <span class="calendar-date-number other-month">1</span>
-                    </td>
-                </tr>
-                </tbody>
-            </table>
         </div>
     </div>
 </section>
