df92f984f01d438300704e124d8d526f733a892e
Ulysses Butler <ulysses@uabutler.com>
2021-04-21 22:36:54 -0500

    Created method for joining friend's meetings

--- common/main.js
+++ common/main.js
@@ -1,6 +1,16 @@
 $(function ()
 {
     $('.hintable').popup();
+    $('.popup-action').popup
+    ({
+       inline: true,
+       hoverable: true,
+       delay:
+           {
+               show: 0,
+               hide: 400
+           }
+    });
     $('.ui.dropdown').dropdown();
     $('.ui.rating.disabled').rating('disable');
     $('.ui.rating.ratable').rating();
