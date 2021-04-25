99b0540eae11f8caec4a9db8f138e4f4620d7b02
Thao Phung <ttp7428@truman.edu>
2021-04-25 10:56:23 -0500

    added edit delete buttons

--- profile-security.php
+++ profile-security.php
@@ -16,7 +16,7 @@
       <div class="four wide column">
         <img class="circular image center" src="assets/avatar.png" style="width: 60%">
         <div class="ui vertical pointing menu">
-        <a class="item" href="profile.php">
+          <a class="item" href="profile.php">
             <span><i class="user icon"> </i>Profile</span>
           </a>
           <!-- <a class="item" href="profile-friends.php">
@@ -39,17 +39,17 @@
 
           <div class="field">
             <label>Confirm Old Password</label>
-            <input type="password" >
+            <input type="password">
           </div>
 
           <div class="field">
             <label>New Password</label>
-            <input type="password" >
+            <input type="password">
           </div>
 
           <div class="field">
             <label>Confirm New Password</label>
-            <input type="password" >
+            <input type="password">
           </div>
 
           <button onclick="window.location='password-success.php'" style="float:right" class="ui button blue">Confirm Change</button>
